95b29257a62d3d09dab7eeb3ac19f14f2a5b827d
Attila <atti80@gmail.com>
2025-04-05 17:21:34 +0100

    aws db connection

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,20 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
 import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
 config({ path: ".env" });
 
 export const db = drizzle({
-  connection: process.env.DATABASE_URL!,
+  connection: {
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PSWD,
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    database: process.env.DATABASE_NAME,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   schema: schema,
 });
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
   out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    host: process.env.DATABASE_HOST!,
+    database: process.env.DATABASE_NAME!,
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PSWD!,
+    port: Number(process.env.DATABASE_PORT!),
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
