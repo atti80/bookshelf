76c418c69f743d8deabb071a90d8fae2300d0b00
Attila <atti80@gmail.com>
2025-04-21 18:09:47 +0100

    genre badge link to filtered list

--- components/Genres.tsx
+++ components/Genres.tsx
@@ -1,4 +1,5 @@
 import { getArticles } from "@/actions/article.actions";
+import Link from "next/link";
 
 type Articles = Awaited<ReturnType<typeof getArticles>>;
 type Genre = Articles["articles"][number]["genres"][number];
@@ -7,12 +8,11 @@ const Genres = ({ genres }: { genres: Genre[] }) => {
   return (
     <div className="flex">
       {genres.map((genre) => (
-        <div
-          key={genre.genreId}
-          className="bg-primary-light rounded-md px-4 py-1 text-sm text-background mr-2"
-        >
-          {genre.genre.name}
-        </div>
+        <Link href={`/?genre=${genre.genreId}`} key={genre.genreId}>
+          <div className="bg-primary-light rounded-md px-4 py-1 text-sm text-background mr-2">
+            {genre.genre.name}
+          </div>
+        </Link>
       ))}
     </div>
   );
