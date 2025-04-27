71fb05e3b5c1784bffcf377cf2940b4e1c1e4250
Attila <atti80@gmail.com>
2025-04-27 10:18:30 +0100

    fix usercard overlap, fix searchbar on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,8 +17,8 @@ const Navbar = async ({
 
   return (
     <header className="w-full sticky top-0">
-      <nav className="h-16 w-full px-8 flex items-center justify-between z-10 bg-background">
-        <div className="flex gap-4">
+      <nav className="h-16 w-full px-2 xs:px-8 flex items-center justify-between z-999 bg-background">
+        <div className="flex gap-4 max-xs:hidden">
           <div className="flex flex-col gap-1">
             <div className="bg-primary-light w-2 h-2"></div>
             <div className="bg-primary w-2 h-2"></div>
@@ -31,7 +31,7 @@ const Navbar = async ({
           </Link>
         </div>
         {showFilters && (
-          <div className="flex gap-2 lg:gap-8">
+          <div className="flex max-xs:grow-1 max-xs:gap-1 gap-2 lg:gap-8">
             <GenreSelectWrapper genres={genres}></GenreSelectWrapper>
             <SearchInputWrapper></SearchInputWrapper>
           </div>
