81b00869f44815e68d0e0c2d23cf7328b9607580
attila <atti80@gmail.com>
2025-08-16 23:08:35 +0200

    env variable renamed aws access key id

--- actions/image.actions.ts
+++ actions/image.actions.ts
@@ -7,7 +7,7 @@ const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   endpoint: process.env.AWS_ENDPOINT,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
