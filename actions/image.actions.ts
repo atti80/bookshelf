9c4433af6d43ef6279f9242e02ebefda510f854c
attila <atti80@gmail.com>
2025-08-16 22:45:55 +0200

    S3 client add parameters

--- actions/image.actions.ts
+++ actions/image.actions.ts
@@ -3,7 +3,9 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
+  forcePathStyle: true,
   region: process.env.AWS_REGION,
+  endpoint: process.env.AWS_ENDPOINT,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
